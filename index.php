4fc32e01e2fd4de863752851386c84e62890fae9
Tom Bedford <tom@developedbytom.com>
2017-08-23 20:32:28 +0100

    returned to original values after testing

--- index.php
+++ index.php
@@ -90,10 +90,36 @@
   
   // Step 5: check with other values
 
-  // declare variables
-  $foo = "Hire";
-  $bar = "Tom";
+  // // declare variables
+  // $foo = "Hire";
+  // $bar = "Tom";
 
+  // for ($i = 1; $i <= 100; $i++) {
+  //   // if the remainder of $i/3 is 0 and $i/5 is 0 i.e. if $i is a multiple of 3 and 5...
+  //   if ($i%3 == 0 && $i%5 == 0) {
+  //     echo $foo." ".$bar;
+  //   } elseif ($i%3 == 0) {
+  //     // if a multiple of 3...
+  //     echo $foo;
+  //   } elseif ($i%5 == 0) {
+  //     // if a multiple of 5...
+  //     echo $bar;
+  //   } else {
+  //     // if not a multiple of 3 and 5, or 3, or 5...
+  //     echo $i;
+  //   }
+  //   // linebreak in every instance
+  //   echo "<br>";
+  // }
+
+
+  // Step 6: returned to original values and checked comments
+
+  // declare variables
+  $foo = "Making";
+  $bar = "Sense";
+
+  // for $i (starting at 1), where $i is less than or equal to 100, incrementing $i each loop...
   for ($i = 1; $i <= 100; $i++) {
     // if the remainder of $i/3 is 0 and $i/5 is 0 i.e. if $i is a multiple of 3 and 5...
     if ($i%3 == 0 && $i%5 == 0) {
