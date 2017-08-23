cc2682f960a16f8af9a0991c05a37994ed5f9d09
Tom Bedford <tom@developedbytom.com>
2017-08-23 20:13:56 +0100

    added first modulo if statement

--- index.php
+++ index.php
@@ -25,21 +25,42 @@
   
   // Step 2: make for loop and check runs from 1 to 100
   
+  // // declare variables
+  // $foo = "Making";
+  // $sense = "Sense";
+
+  // for ($i = 1; $i <= 100; $i++) {
+  //   // if i/3 and i/5
+  //     // echo foo bar
+  //   // elseif i/3
+  //     // echo foo
+  //   // elseif i/5
+  //     // echo $bar
+  //   // else
+  //   echo $i."<br>";
+  // }
+
+
+  // Step 3: added first modulo if statement
+
   // declare variables
   $foo = "Making";
   $sense = "Sense";
 
   for ($i = 1; $i <= 100; $i++) {
-    // if i/3 and i/5
-      // echo foo bar
-    // elseif i/3
-      // echo foo
-    // elseif i/5
-      // echo $bar
-    // else
-    echo $i."<br>";
+    // if the remainder of $i/3 is 0 i.e. if $i is a multiple of 3...
+    if ($i%3 == 0) {
+      echo $foo."<br>";
+      // if i/3 and i/5
+        // echo foo bar
+      // elseif i/3
+        // echo foo
+      // elseif i/5
+        // echo $bar
+    } else {
+      echo $i."<br>";
+    }
   }
-
 
 ?>
 
