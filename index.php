c39d143ee47ec6303820573ee8d8f2e8f4363e20
Tom Bedford <tom@developedbytom.com>
2017-08-23 20:20:07 +0100

    added remaining if/else checks and corrected bar variable name

--- index.php
+++ index.php
@@ -13,14 +13,14 @@
   // foo = making
   // bar = sense
   // for i < 100
-    // if i/3 and i/5
-      // echo foo bar
-    // elseif i/3
-      // echo foo
-    // elseif i/5
-      // echo $bar
-    // else
-      // echo i
+  //   if i/3 and i/5
+  //     echo foo bar
+  //   elseif i/3
+  //     echo foo
+  //   elseif i/5
+  //     echo $bar
+  //   else
+  //     echo i
   
   
   // Step 2: make for loop and check runs from 1 to 100
@@ -43,23 +43,48 @@
 
   // Step 3: added first modulo if statement
 
+  // // declare variables
+  // $foo = "Making";
+  // $sense = "Sense";
+
+  // for ($i = 1; $i <= 100; $i++) {
+  //   // if the remainder of $i/3 is 0 i.e. if $i is a multiple of 3...
+  //   if ($i%3 == 0) {
+  //     echo $foo."<br>";
+  //     // if i/3 and i/5
+  //       // echo foo bar
+  //     // elseif i/3
+  //       // echo foo
+  //     // elseif i/5
+  //       // echo $bar
+  //   } else {
+  //     echo $i."<br>";
+  //   }
+  // }
+
+
+  // Step 4: added remaining if/else checks
+
   // declare variables
   $foo = "Making";
-  $sense = "Sense";
+  $bar = "Sense";
 
   for ($i = 1; $i <= 100; $i++) {
-    // if the remainder of $i/3 is 0 i.e. if $i is a multiple of 3...
-    if ($i%3 == 0) {
-      echo $foo."<br>";
-      // if i/3 and i/5
-        // echo foo bar
-      // elseif i/3
-        // echo foo
-      // elseif i/5
-        // echo $bar
+    // if the remainder of $i/3 is 0 and $i/5 is 0 i.e. if $i is a multiple of 3 and 5...
+    if ($i%3 == 0 && $i%5 == 0) {
+      echo $foo." ".$bar;
+    } elseif ($i%3 == 0) {
+      // if a multiple of 3...
+      echo $foo;
+    } elseif ($i%5 == 0) {
+      // if a multiple of 5...
+      echo $bar;
     } else {
-      echo $i."<br>";
+      // if not a multiple of 3 and 5, or 3, or 5...
+      echo $i;
     }
+    // linebreak in every instance
+    echo "<br>";
   }
 
 ?>
