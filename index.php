6a9254e73f3474ec4a15bd3555bcd3eb7bff6f5a
Tom Bedford <tom@developedbytom.com>
2017-08-23 20:07:38 +0100

    added variables and loop from 1 to 100

--- index.php
+++ index.php
@@ -7,6 +7,8 @@
 <body>
   
 <?php 
+
+  // Step 1: write out proposed structure
   
   // foo = making
   // bar = sense
@@ -19,7 +21,26 @@
       // echo $bar
     // else
       // echo i
-      
+  
+  
+  // Step 2: make for loop and check runs from 1 to 100
+  
+  // declare variables
+  $foo = "Making";
+  $sense = "Sense";
+
+  for ($i = 1; $i <= 100; $i++) {
+    // if i/3 and i/5
+      // echo foo bar
+    // elseif i/3
+      // echo foo
+    // elseif i/5
+      // echo $bar
+    // else
+    echo $i."<br>";
+  }
+
+
 ?>
 
 </body>
