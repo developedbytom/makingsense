19657b476164b175aeec681d13bba11b307d986d
Tom Bedford <tom@developedbytom.com>
2017-08-23 20:01:07 +0100

    written out problem

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>Inspira: Making Sense</title>
+</head>
+<body>
+  
+<?php 
+  
+  // foo = making
+  // bar = sense
+  // for i < 100
+    // if i/3 and i/5
+      // echo foo bar
+    // elseif i/3
+      // echo foo
+    // elseif i/5
+      // echo $bar
+    // else
+      // echo i
+      
+?>
+
+</body>
+</html>
